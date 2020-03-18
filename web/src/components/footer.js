d6f6c011daf08372fe5f2d6a11234c8b1f88fbdd
Tony Heimark <tjh-94@hotmail.com>
2020-03-18 16:07:49 +0100

    added server.js and procfile

---
+++ web/src/components/footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import coronaPattern from '../assets/corona_pattern.svg';
+import smiley from '../assets/smiley.svg';
+
+const Footer = () => {
+  return (
+    <div className="module footer">
+      <div className="footer__content">
+        <div className="footer__column">
+          <h4>Wemunity project</h4>
+          <p>
+            Wemunity is an open source project where we explore how immunized
+            citizens can be utilized strategically in response to viral
+            epidemics.
+          </p>
+        </div>
+        <div className="footer__image">
+          <img
+            className="footer__corona"
+            src={coronaPattern}
+            alt="corona pattern"
+          />
+          <img className="footer__smiley" src={smiley} alt="smiley" />
+        </div>
+        <div className="footer__column">
+          <h4>Contact</h4>
+          <span>Anders Waage Nilsen</span>
+          <span>anders@new.no</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
