6ee167e7d48063c95a05549bc4f3cec389ca71c3
Tony Heimark <tjh-94@hotmail.com>
2020-03-26 14:59:43 +0100

    added validation to signup screens

--- web/src/pages/onboarding/signup2.js
+++ web/src/pages/onboarding/signup2.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-
-import { setSymptomStartDate, setSymptomEndDate, setAgreeTerms } from '../../state/onboarding';
+import {
+  setSymptomStartDate,
+  setSymptomEndDate,
+  setAgreeTerms
+} from '../../state/onboarding';
 
 import DateField from '../../components/bits/datefield';
 import Dots from '../../components/bits/dots';
@@ -13,53 +17,91 @@ import Smileyneutral from '../../assets/smiley_neutral.svg';
 import WemunityIconLight from '../../assets/wemunity-icon-light.svg';
 
 const Signup2 = props => {
-
-  const onBoardingState = useSelector(state => state.onboarding)
-  const dispatch = useDispatch()
-
-  console.dir(onBoardingState)
-
+  const onBoardingState = useSelector(state => state.onboarding);
+  const dispatch = useDispatch();
   const [showTerms, setShowTerms] = useState(false);
+  const [redirect, setRedirect] = useState(false);
+  const [errors, setErrors] = useState(false);
 
-  const handleShowTerms = (name) => {
+  const handleShowTerms = name => {
     // console.log(name);
     setShowTerms(!showTerms);
   };
 
-  const onDateChange = (date) => {
+  const onDateChange = date => {
     console.log(date);
-  }
+  };
 
+  const handleValidation = () => {
+    if (onBoardingState.agreeTerms) {
+      setRedirect(<Redirect push to="/signup/3" />);
+    } else {
+      setErrors({
+        ...errors,
+        terms: 'You must accept the terms and conditions before continuing'
+      });
+    }
+  };
 
   return (
     <div className="signup2">
-      <img className="wemunity-icon" src={WemunityIconLight} alt="Ø"/>
+      <img className="wemunity-icon" src={WemunityIconLight} alt="Ø" />
       <div className="signup2__wrapper">
+        {errors.terms && !onBoardingState.agreeTerms && (
+          <span className="signup2__error">{errors.terms}</span>
+        )}
         <div className="signup2__top">
-
-          <img className="signup2__smiley" src={Smileyneutral} alt=":|"/>
+          <img className="signup2__smiley" src={Smileyneutral} alt=":|" />
         </div>
         <div className="signup2__content">
           <div className="signup2__form">
             <div className="signup2__form-field">
-              <DateField value={onBoardingState.symptomStartDate} onChange={(e) => dispatch(setSymptomStartDate(e))} lightText={true} topText={'When did you start showing symptoms?'} arrow={true} date={true}/>
+              <DateField
+                value={onBoardingState.symptomStartDate}
+                onChange={e => dispatch(setSymptomStartDate(e))}
+                lightText={true}
+                topText={'When did you start showing symptoms?'}
+                arrow={true}
+                date={true}
+              />
             </div>
             <div className="signup2__form-field">
-              <DateField value={onBoardingState.symptomEndDate} onChange={(e) => dispatch(setSymptomEndDate(e))} lightText={true} topText={'When did you last show symptoms?'} arrow={true} date={true}/>
+              <DateField
+                value={onBoardingState.symptomEndDate}
+                onChange={e => dispatch(setSymptomEndDate(e))}
+                lightText={true}
+                topText={'When did you last show symptoms?'}
+                arrow={true}
+                date={true}
+              />
             </div>
           </div>
           <div className="signup2__terms">
             <div>
-              <Checkbox value={onBoardingState.agreeTerms} onChange={(val) => dispatch(setAgreeTerms(val))} text={'I agree to the'} caption=" "/>
+              <Checkbox
+                value={onBoardingState.agreeTerms}
+                onChange={val => dispatch(setAgreeTerms(val))}
+                text={'I agree to the'}
+                caption=" "
+              />
             </div>
-            <span onClick={handleShowTerms}>&nbsp;<a>terms and conditions</a></span>
-            {showTerms ? <Terms onClick={handleShowTerms}/> : null}
+            <span onClick={handleShowTerms}>
+              &nbsp;<a>terms and conditions</a>
+            </span>
+            {showTerms ? <Terms onClick={handleShowTerms} /> : null}
           </div>
         </div>
         <div className="signup2__bottom">
-          <Button text={'Next'} light={false} link={'/signup/3'}/>
+          <Button
+            text={'Next'}
+            light={false}
+            link={'/signup/3'}
+            handleValidation={handleValidation}
+          />
+
+          {redirect}
           <div className="signup2__dots">
-            <Dots active="1"/>
+            <Dots active="1" />
           </div>
         </div>
       </div>
@@ -69,4 +111,4 @@ const Signup2 = props => {
 
 export default Signup2;
 
-          // <img className="signup2__corona-image" src={Coronapattern} alt="pattern"/>
+// <img className="signup2__corona-image" src={Coronapattern} alt="pattern"/>
--- web/src/pages/onboarding/signup4.js
+++ web/src/pages/onboarding/signup4.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
-import { setName, setLocation, setAge, setDriversLicense } from '../../state/onboarding';
+import {
+  setName,
+  setLocation,
+  setAge,
+  setDriversLicense
+} from '../../state/onboarding';
 
 import FormField from '../../components/bits/formfield';
 import Dots from '../../components/bits/dots';
@@ -13,45 +19,109 @@ import WemunityIconDark from '../../assets/wemunity-icon-dark.svg';
 
 //When did you start showing symptoms?
 
-
-
 const Signup4 = props => {
+  const onBoardingState = useSelector(state => state.onboarding);
+  const dispatch = useDispatch();
+  const [errorState, setErrorState] = useState({});
+  const [redirect, setRedirect] = useState(false);
 
-  const onBoardingState = useSelector(state => state.onboarding)
-  const dispatch = useDispatch()
+  console.dir(onBoardingState);
 
-  console.dir(onBoardingState)
+  const handleValidation = () => {
+    let errors = {};
+    if (onBoardingState.name.trim() === '') {
+      errors = { ...errors, name: 'This field cant be empty' };
+    }
+    if (onBoardingState.location.trim() === '') {
+      errors = { ...errors, location: 'This field cant be empty' };
+    }
+    if (onBoardingState.age.trim() === '') {
+      errors = { ...errors, age: 'This field cant be empty' };
+    }
+
+    const errorCheck = Object.keys(errors);
+    if (errorCheck.length === 0) {
+      setRedirect(<Redirect push to="/signup/5" />);
+    } else {
+      setErrorState(errors);
+    }
+  };
 
   return (
     <div className="signup4">
-      <img className="wemunity-icon" src={WemunityIconDark} alt="Ø"/>
+      <img className="wemunity-icon" src={WemunityIconDark} alt="Ø" />
       <div className="signup4__wrapper">
         <div className="signup4__top">
           <div className="signup4__profileimage">
-            <img src={CrossIcon} alt="+"/>
+            <img src={CrossIcon} alt="+" />
           </div>
-          <div className="signup4__image-text"><span>Upload profile picture</span></div>
+          <div className="signup4__image-text">
+            <span>Upload profile picture</span>
+          </div>
         </div>
         <div className="signup4__content">
           <div className="signup4__form">
-            <div className="signup4__form-field">
-              <FormField value={onBoardingState.name} topText={'My name'} placeholderText={'Jon Doe'} onChange={(e) => dispatch(setName(e.target.value))}/>
+            <div
+              className={`signup4__form-field ${errorState.name &&
+                'signup4__form-field--paddingTop'}`}
+            >
+              {errorState.name && (
+                <span className="signup4__error">{errorState.name}</span>
+              )}
+              <FormField
+                value={onBoardingState.name}
+                topText={'My name'}
+                placeholderText={'Jon Doe'}
+                onChange={e => dispatch(setName(e.target.value))}
+              />
             </div>
-            <div className="signup4__form-field">
-              <FormField value={onBoardingState.location} topText={'Where do you live?'} placeholderText={'Tøyen'} onChange={(e) => dispatch(setLocation(e.target.value))}/>
+            <div
+              className={`signup4__form-field ${errorState.location &&
+                'signup4__form-field--paddingTop'}`}
+            >
+              {errorState.location && (
+                <span className="signup4__error">{errorState.location}</span>
+              )}
+              <FormField
+                value={onBoardingState.location}
+                topText={'Where do you live?'}
+                placeholderText={'Tøyen'}
+                onChange={e => dispatch(setLocation(e.target.value))}
+              />
             </div>
-            <div className="signup4__form-field">
-              <FormField value={onBoardingState.age} topText={'How old are you?'} placeholderText={'30'} onChange={(e) => dispatch(setAge(e.target.value))}/>
+            <div
+              className={`signup4__form-field ${errorState.age &&
+                'signup4__form-field--paddingTop'}`}
+            >
+              {errorState.age && (
+                <span className="signup4__error">{errorState.age}</span>
+              )}
+              <FormField
+                value={onBoardingState.age}
+                topText={'How old are you?'}
+                placeholderText={'30'}
+                onChange={e => dispatch(setAge(e.target.value))}
+              />
             </div>
           </div>
           <div className="signup4__radiofield">
-            <RadioButton value={onBoardingState.driversLicense} text="I have my drivers license" onChange={(val) => dispatch(setDriversLicense(val))}/>
+            <RadioButton
+              value={onBoardingState.driversLicense}
+              text="I have my drivers license"
+              onChange={val => dispatch(setDriversLicense(val))}
+            />
           </div>
         </div>
         <div className="signup4__bottom">
-          <Button text={'Next'} light={false} link={'/signup/5'}/>
+          <Button
+            handleValidation={handleValidation}
+            text={'Next'}
+            light={false}
+            link={'/signup/5'}
+          />
+          {redirect}
           <div className="signup3__dots">
-            <Dots active="3"/>
+            <Dots active="3" />
           </div>
         </div>
       </div>
