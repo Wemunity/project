bffc78c52976a85c5d5445c2689df46ac56c8db2
Erik Borge <31848774+ErikBorge@users.noreply.github.com>
2020-03-27 16:44:52 +0100

    Revert "Onboarding"

--- web/src/index.js
+++ web/src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routes/appRouter';
-import ReduxWrapper from './state/reduxWrapper'
 
-ReactDOM.render(
-  <ReduxWrapper>
-    <AppRouter />
-  </ReduxWrapper>, document.getElementById('root'));
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
--- web/src/routes/appRouter.js
+++ web/src/routes/appRouter.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LandingPage from '../pages/landingPage.js';
-import Signup from '../pages/onboarding/signup';
-import Signup1 from '../pages/onboarding/signup1';
-import Signup2 from '../pages/onboarding/signup2';
-import Signup3 from '../pages/onboarding/signup3';
-import Signup4 from '../pages/onboarding/signup4';
-import Signup5 from '../pages/onboarding/signup5';
-import Welcome from '../pages/onboarding/welcome';
-import Tasks from '../pages/tasks';
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
       <Switch>
         <Route path="/" component={LandingPage} exact={true} />
-        <Route path="/signup" component={Signup} exact={true} />
-        <Route path="/signup/1" component={Signup1} exact={true} />
-        <Route path="/signup/2" component={Signup2} exact={true} />
-        <Route path="/signup/3" component={Signup3} exact={true} />
-        <Route path="/signup/4" component={Signup4} exact={true} />
-        <Route path="/signup/5" component={Signup5} exact={true} />
-        <Route path="/welcome" component={Welcome} exact={true} />
-        <Route path="/tasks" component={Tasks} exact={true} />
       </Switch>
     </div>
   </BrowserRouter>
