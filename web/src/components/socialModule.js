3ba3b1682a0930da3ba4d1ef0ed6d3a264d87a5c
“ErikBorge” <erikaasborge@gmail.com>
2020-03-30 14:44:01 +0200

    added spleis iframe

--- web/src/components/socialModule.js
+++ web/src/components/socialModule.js
@@ -14,21 +14,25 @@ const SocialModule = ({ m }) => {
         <div className="social-module__text">
           {m && m[0].text && <BlockContent blocks={m[0].text} />}
         </div>
-        <div className="social-module__buttons">
-          <a
-            href={m && m[0].buttonBlueUrl}
-            className="social-module__button social-module__button--blue"
-          >
-            <span>{m && m[0].buttonBlueText}</span>
-            <img src={facebook} alt="facebook" />
-          </a>
-          <a
-            href={m && m[0].buttonGreyUrl}
-            className="social-module__button social-module__button--grey"
-          >
-            <span>{m && m[0].buttonGreyText}</span>
-            <img src={github} alt="github" />
-          </a>
+        <div className="social-module__main">
+          <iframe height="450px" className="social-module__iframe" title="Spleis" src="https://www.spleis.no/project/101957/embed" frameBorder="0"></iframe>
+          <div className="social-module__buttons">
+
+            <a
+              href={m && m[0].buttonBlueUrl}
+              className="social-module__button social-module__button--blue"
+            >
+              <span>{m && m[0].buttonBlueText}</span>
+              <img src={facebook} alt="facebook" />
+            </a>
+            <a
+              href={m && m[0].buttonGreyUrl}
+              className="social-module__button social-module__button--grey"
+            >
+              <span>{m && m[0].buttonGreyText}</span>
+              <img src={github} alt="github" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
