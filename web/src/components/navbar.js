df1e6c5d04f5c519ca79e7dc74b36b356a8628a2
spenceruns <spenceruns@gmail.com>
2020-04-02 06:07:43 +0000

    Created navbar and sidebar for mobile devices

--- web/src/components/navbar.js
+++ web/src/components/navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import wemunityLogoDark from '../assets/wemunity-icon-dark.svg'
 import wemunityLogoLight from '../assets/wemunity-icon-light.svg'
@@ -6,13 +6,17 @@ import menuBarsLight from '../assets/menu-bars-light.svg'
 import menuBarsDark from '../assets/menu-bars-dark.svg'
 
 export default function NavBar(props) {
+   const [menuOpen, setMenuOpen] = useState(false)
    const theme = props.theme === "dark" ? "dark" : "light"
    return (
       <div className={`navbar ${theme}`}>
-         {/* <img
+         <SideBar theme={theme} menuOpen={menuOpen} toggleMenu={() => setMenuOpen(!menuOpen)} {...props} />
+         <img
             src={props.theme === "dark" ? menuBarsLight : menuBarsDark }
+            className="navbar__bars"
             alt="Menu Bars"
-         /> */}
+            onClick={() => setMenuOpen(!menuOpen)}
+         />
          <NavLink to={"/"} className="navbar__logo navbar__link">
             <img
                src={props.theme === "dark" ? wemunityLogoLight : wemunityLogoDark}
@@ -21,12 +25,29 @@ export default function NavBar(props) {
             <h2 className={`navbar__link ${theme}`}>Wemunity project</h2>
          </NavLink>
          <nav className="navbar__links">
-            <NavLink to={"/about"} className={`navbar__link ${props.location.pathname === "/about" && "bold"} ${theme}`}>About</NavLink>
-            <NavLink to={"/press"} className={`navbar__link ${props.location.pathname === "/press" && "bold"} ${theme}`}>Press</NavLink>
-            <NavLink to={"/partners"} className={`navbar__link  ${props.location.pathname === "/partners" && "bold"} ${theme}`}>Partners and Support</NavLink>
-            <NavLink to={"/story"} className={`navbar__link ${props.location.pathname === "/story" && "bold"} ${theme}`}>The Story</NavLink>
-            <NavLink to={"/platform"} className={`navbar__link ${props.location.pathname === "/platform" && "bold"} ${theme}`}>The Platform</NavLink>
+            <NavLink to={"/about"} className={`navbar__link ${props.location.pathname === "/about" ? "bold" : ""} ${props.theme}`}>About</NavLink>
+            <NavLink to={"/press"} className={`navbar__link ${props.location.pathname === "/press" ? "bold" : ""} ${props.theme}`}>Press</NavLink>
+            <NavLink to={"/partners"} className={`navbar__link  ${props.location.pathname === "/partners" ? "bold" : ""} ${props.theme}`}>Partners and Support</NavLink>
+            <NavLink to={"/story"} className={`navbar__link ${props.location.pathname === "/story" ? "bold" : ""} ${props.theme}`}>The Story</NavLink>
+            <NavLink to={"/platform"} className={`navbar__link ${props.location.pathname === "/platform" ? "bold" : ""} ${props.theme}`}>The Platform</NavLink>
          </nav>
       </div>
+   )
+}
+
+function SideBar(props) {
+   return (
+      <React.Fragment>
+         <div className={`sidebar ${props.menuOpen ? "opened" : ""} ${props.theme === "dark" ? "dark" : "light"}`}>
+            <nav className="navbar__links sidebar__links">
+               <NavLink to={"/about"} className={`navbar__link ${props.location.pathname === "/about" ? "bold" : ""} ${props.theme}`}>About</NavLink>
+               <NavLink to={"/press"} className={`navbar__link ${props.location.pathname === "/press" ? "bold" : ""} ${props.theme}`}>Press</NavLink>
+               <NavLink to={"/partners"} className={`navbar__link  ${props.location.pathname === "/partners" ? "bold" : ""} ${props.theme}`}>Partners and Support</NavLink>
+               <NavLink to={"/story"} className={`navbar__link ${props.location.pathname === "/story" ? "bold" : ""} ${props.theme}`}>The Story</NavLink>
+               <NavLink to={"/platform"} className={`navbar__link ${props.location.pathname === "/platform" ? "bold" : ""} ${props.theme}`}>The Platform</NavLink>
+            </nav>
+         </div>
+         <div className={`shadow ${props.menuOpen ? "opened" : ""}`} onClick={props.toggleMenu}></div>
+      </React.Fragment>
    )
 }
