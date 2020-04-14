76cdd1660bab6cfe7ca997349e98434be6aa588f
spenceruns <spenceruns@gmail.com>
2020-04-14 00:17:28 +0000

    Added final styling for press photos and download buttons. Buttons do not download at the moment

--- web/src/components/pressKitModule.js
+++ web/src/components/pressKitModule.js
@@ -7,7 +7,17 @@ export default function PressKitModule({ data }) {
   console.log(data)
   return (
     <div className="press-kit">
-      {data.pressKitImages.map(image => <PressPhoto key={image._key} {...{image}} />)}
+      <a
+        className="button press-kit_button press-kit_all"
+        href={``}
+        download>
+        <div className="button_content">
+          <span>Download All</span>
+        </div>
+      </a>
+      <div className="press-kit_container">
+        {data.pressKitImages.map(image => <PressPhoto key={image._key} {...{image}} />)}
+      </div>
     </div>
   )
 }
